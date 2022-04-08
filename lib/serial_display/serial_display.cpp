d2872cb93441b7d307f799e1caea0cbc3380bf50
vanceb <vance@axxe.co.uk>
2022-04-08 18:20:08 +0100

    Added onboard LED to serial display task

--- lib/serial_display/serial_display.cpp
+++ lib/serial_display/serial_display.cpp
@@ -1,7 +1,13 @@
+#include <Arduino.h>
 #include <serial_display.h>
 #include <WiFi.h>
 #include <keeptime.h>
 #include <uptime_formatter.h>
+
+#define LED_PIN 2
+#define LED_CHANNEL 0
+#define FREQ 5000
+#define RESOLUTION 8
 
 void display(void * parameters) {
 
@@ -14,11 +20,23 @@ void display(void * parameters) {
         }
         Serial.println("\nConnected!");
     }
+
+    /* Configure onboard LED */
+    ledcSetup(LED_CHANNEL, FREQ, RESOLUTION);
+    ledcAttachPin(LED_PIN,LED_CHANNEL);
+    
     
     /* Infinite loop needed for FreeRTOS Task */
+    unsigned long counter = 0;
     while (true) {
-        Serial.print(getEpochStringByParams(UK.toLocal(now())));
-        Serial.println((" up " + uptime_formatter::getUptime()));
-        delay(1000);
+        if (counter % 100 == 0) {
+            /* Do something every second */
+            Serial.print(getEpochStringByParams(UK.toLocal(now())));
+            Serial.println((" up " + uptime_formatter::getUptime()));
+        }
+        uint8_t brightness = (counter % 128) > 64 ? 128 - counter % 128 : counter % 128;
+        ledcWrite(LED_CHANNEL, brightness);
+        ++counter;
+        delay(10);
     }
 }
